57e9072830a765c8d528a3b14396cca425db5683
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-01-29 17:51:28 -0500

    test: fixup rateLimit -> min/maxDebtIncrease parameters

--- tests/functional/strategy/test_fees.py
+++ tests/functional/strategy/test_fees.py
@@ -12,7 +12,7 @@ def test_performance_fees(gov, vault, token, TestStrategy, rewards, strategist):
     assert vault.balanceOf(strategist) == 0
 
     strategy = strategist.deploy(TestStrategy, vault)
-    vault.addStrategy(strategy, 2_000, 1000, 50, {"from": gov})
+    vault.addStrategy(strategy, 2_000, 1000, 1000, 50, {"from": gov})
     token.transfer(strategy, 10 ** 8, {"from": gov})
     strategy.harvest({"from": strategist})
 
@@ -28,7 +28,7 @@ def test_zero_fees(gov, vault, token, TestStrategy, rewards, strategist):
     assert vault.balanceOf(strategist) == 0
 
     strategy = strategist.deploy(TestStrategy, vault)
-    vault.addStrategy(strategy, 2_000, 1000, 0, {"from": gov})
+    vault.addStrategy(strategy, 2_000, 1000, 1000, 0, {"from": gov})
     token.transfer(strategy, 10 ** 8, {"from": gov})
     strategy.harvest({"from": strategist})
 
@@ -54,10 +54,10 @@ def test_max_fees(gov, vault, token, TestStrategy, rewards, strategist):
     # addStrategy should check for MAX FEE
     strategy = strategist.deploy(TestStrategy, vault)
     with brownie.reverts():
-        vault.addStrategy(strategy, 2_000, 1000, FEE_MAX + 1, {"from": gov})
+        vault.addStrategy(strategy, 2_000, 1000, 1000, FEE_MAX + 1, {"from": gov})
 
     # updateStrategyPerformanceFee should check for max to be MAX FEE - current performance fee
-    vault.addStrategy(strategy, 2_000, 1000, 0, {"from": gov})
+    vault.addStrategy(strategy, 2_000, 1000, 1000, 0, {"from": gov})
     vault_performance_fee = vault.performanceFee()
     with brownie.reverts():
         vault.updateStrategyPerformanceFee(
